411b7ed13e8d90f5e9eec28e1ea31c11b9e8c84f
giseok0728 <164345681+giseok0728@users.noreply.github.com>
2024-10-07 15:31:28 +0900

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -45,3 +45,46 @@ $(window).scroll(function() {
     $('.top_text_area').removeClass('dis_none');
   }
 });
+
+
+
+// 사이드 슬라이더 이벤트
+
+$(".open_side_slide").click(function(){
+  $(".m_side_slide").show();
+  $(".open_side_slide").hide();
+  $(".close_side_slide").show();
+});
+
+$(".close_side_slide").click(function(){
+  $(".m_side_slide").hide();
+  $(".close_side_slide").hide();
+  $(".open_side_slide").show();
+});
+
+
+
+
+/* 메뉴 접히고 펼치기 */
+$('.side_li').click(function(e){
+  //console.log("메뉴 클릭됨");
+  
+  /* 만약 클릭된 메뉴에 엑티브 클래스가 있으면 */
+  if ( $(this).hasClass('active') ){
+      /* 클릭된 메뉴의 엑티브를 없앤다 */
+      $(this).removeClass('active');
+  }
+  else {
+      /* 클릭된 메뉴의 형제의 엑티브를 없앤다 */
+      $(this).siblings('.active').removeClass('active');
+      
+      /* 클릭된 메뉴(지역)의 엑티브를 없앤다 */
+      $(this).find('.active').removeClass('active');
+      
+      /* 클릭된 메뉴의 엑티브를 만든다 */
+      $(this).addClass('active');
+  }
+  
+  /* 클릭된 메뉴 안에 다른 메뉴를 클릭하면 위에있는 메뉴가 같이 클릭되는데 그것을 막아준다 */
+  e.stopPropagation();
+});
