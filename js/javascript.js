a21a9d53be765860c321a48bb50afa85940aff3d
giseok0728 <164345681+giseok0728@users.noreply.github.com>
2024-10-07 13:46:00 +0900

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     loop: true,
-    // autoplay: { delay: 3000, disableOnInteraction: false },
+    autoplay: { delay: 3000, disableOnInteraction: false },
     pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
